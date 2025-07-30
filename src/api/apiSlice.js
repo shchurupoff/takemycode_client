2513b4eced583acd7963bbd2fd6b17ef0c1656db
Ivan <vanya.schurupov@yandex.ru>
2025-07-30 20:24:19 +0300

    change api url

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://takemycode-server-bmkf.vercel.app/api",\
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://takemycode-server-bmkf.vercel.app/api",\
+    // baseUrl: "http://localhost:5000/api",
   }),
   tagTypes: ["Items", "State"],
   endpoints: (builder) => ({
